4fa3823391b0472ee4da64fd81a2296ca7a96fd3
karimLamari <l.karim9942@gmail.com>
2025-10-22 16:07:17 +0200

    ajout sharp pour optimisation d'image

--- backend/src/middleware/multer-config.js
+++ backend/src/middleware/multer-config.js
@@ -1,22 +1,47 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images'); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
-    cb(null, true); 
+    cb(null, true);
   } else {
     cb(new Error('Seules les images sont acceptées !'), false);
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const storage = multer.memoryStorage();
 
-module.exports = upload.single('image'); 
+const uploadMulter = multer({ storage, fileFilter }).single('image');
+
+
+const upload = (req, res, next) => {
+  uploadMulter(req, res, async (err) => {
+    if (err) return res.status(400).json({ error: err.message });
+    if (!req.file) return next();
+
+    try {
+      const outputDir = 'images';
+      if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+      const filename = Date.now() + '.webp';
+      const outputPath = path.join(outputDir, filename);
+
+      await sharp(req.file.buffer)
+        .resize({ width: 800 }) 
+        .webp({ quality: 80 })  
+        .toFile(outputPath);
+
+      req.file.filename = filename;
+
+      next();
+    } catch (error) {
+      console.error('Erreur Sharp :', error);
+      res.status(500).json({ error: "Erreur lors du traitement de l'image." });
+    }
+  });
+};
+
+module.exports = upload;
